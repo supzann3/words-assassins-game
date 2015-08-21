ec9d7a34e91d516650597814e7abe6bb330d6938
nancyhawa <hawa.nancy@gmail.com>
2015-08-21 13:23:03 -0400

    add victim assignment methods

--- app/models/players.rb
+++ app/models/players.rb
@@ -1,3 +1,28 @@
 class Player < ActiveRecord::Base
 
+  # def assign_victim_id
+  #   x = self.id + 1
+  #   loop do
+  #     if Player(x % Player.all.size).alive? == TRUE
+  #       return Player(x % Player.all.size).alive?
+  #     end
+  #     x += 1
+  #     if x == Player.all.size
+  #       id_looper = 1
+  #     end
+  #   end
+  # end
+
+  def victim_id
+    id < Player.all.size ? id + 1 : 1
+  end
+
+  def reassign_victim_upon_successful_assassination
+    victim_id = victim.victim_id
+  end
+
+  def victim_name
+    Player(victim_id).name
+  end
+
 end
