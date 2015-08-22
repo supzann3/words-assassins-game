ee5273ce820e49f5eeb61154e79faf3371793317
supzann3 <susan.hehe01@gmail.com>
2015-08-22 14:21:14 -0400

    update my files

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,53 +1,60 @@
 require 'gmail'
 class Email #<<ActiveRecord::Base # not sure if we should create a table for email
- # belongs_to :player
+# belongs_to :player
 
-  def deliver_gmail(recipient, subject, body)
-  #  gmail=Gmail.connect("email", "password") do |gmail|
-    gmail.deliver do
-      # binding.pry email=Mail.new
-      to "#{recipient}"
-      subject "#{subject}"
-      text_part do
-        body "#{body}"
-      end
-      html_part do
-       content_type 'text/html; charset=UTF-8'
-       body "<p>Text of <em>html</em> message.</p>" # add html part
+   def initialize
+     keys = YAML.load_file('keys.yml')
+     @gmail = Gmail.connect(keys['EMAIL'], keys['PASSWORD'])
+   end
+
+   def deliver_gmail(recipient, subject, body)
+     @gmail.deliver do
+       # binding.pry email=Mail.new
+       to "#{recipient}"
+       subject "#{subject}"
+       text_part do
+         body "#{body}"
+       end
+       # html_part do
+       #  content_type 'text/html; charset=UTF-8'
+       #  body "<p>Text of <em>html</em> message.</p>" # add html part
+     #  end
+       # add_file "/path/to/some_image.jpg" add image
      end
-      # add_file "/path/to/some_image.jpg" add image
-    end
-  # end
-  end
+   end
 
-  def initial_email(player_email,word,victim) #comes from Player
-    # players_email.each do |player| #either to a iteration at Player's class or pass an array here
-      recipient = player_email
-      subject = "Your secret mission."
-      body = "You have been assigned to kill #{victim}. \n You can accomplish this by getting your victim to say the word #{word}"
-      deliver_gmail(recipient, subject, body)
-      puts "Sent e-mail to #{player_email}."
-    # end
-  end
-  def reassigning_email(player_email,word,victim) #comes from Player
-      recipient = player_email
-      subject = "Your new victim"
-      body = "You have a new victim to kill #{victim}. \n You can accomplish this by getting your victim to say the word #{word}"
-      deliver_gmail(recipient, subject, body)
-      puts "Sent e-mail to #{player_email}."
-  end
-  def dead_email(player_email,killer_name) #comes from Player
-    recipient = player_email
-    subject = "Your have been killed"
-    body = "You have been killed by #{killer_name}, better luck next time. \n Thank you for playing !"
-    deliver_gmail(recipient, subject, body)
-    puts "Sent e-mail to #{player_email}."
-  end
-  def winner_email(player_email) #comes from Player
-    recipient = player_email
-    subject = "You are a winner"
-    body = "You are the last suvivor, You are the ultimate Winner!"
-    deliver_gmail(recipient, subject, body)
-    puts "Sent e-mail to #{player_email}."
-  end
+
+ def initial_email(player_email,word,victim) #comes from Player
+   # players_email.each do |player| #either to a iteration at Player's class or pass an array here
+     recipient = player_email
+     subject = "Your secret mission."
+     body = "You have been assigned to kill #{victim}. \n You can accomplish this by getting your victim to say the word #{word}"
+     deliver_gmail(recipient, subject, body)
+     puts "Sent e-mail to #{player_email}."
+   # end
+ end
+
+ def reassigning_email(player_email,word,victim) #comes from Player
+     recipient = player_email
+     subject = "Your new victim"
+     body = "You have a new victim to kill #{victim}. \n You can accomplish this by getting your victim to say the word #{word}"
+     deliver_gmail(recipient, subject, body)
+     puts "Sent e-mail to #{player_email}."
+ end
+
+ def dead_email(player_email,killer_name) #comes from Player
+   recipient = player_email
+   subject = "Your have been killed"
+   body = "You have been killed by #{killer_name}, better luck next time. \n Thank you for playing !"
+   deliver_gmail(recipient, subject, body)
+   puts "Sent e-mail to #{player_email}."
+ end
+
+ def winner_email(player_email,winner_name) #comes from Player
+   recipient = player_email
+   subject = "The winner has been announced"
+   body = "The winner of the game is #{winner_name}"
+   deliver_gmail(recipient, subject, body)
+   puts "Sent e-mail to #{player_email}."
+ end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,93 +1,93 @@
 class Player < ActiveRecord::Base
-  # include Word
-  # def assign_victim_id
-  #   x = self.id + 1
-  #   loop do
-  #     if Player(x % Player.all.size).alive? == TRUE
-  #       return Player(x % Player.all.size).alive?
-  #     end
-  #     x += 1
-  #     if x == Player.all.size
-  #       id_looper = 1
-  #     end
-  #   end
-  # end
+ # include Word
+ # def assign_victim_id
+ #   x = self.id + 1
+ #   loop do
+ #     if Player(x % Player.all.size).alive? == TRUE
+ #       return Player(x % Player.all.size).alive?
+ #     end
+ #     x += 1
+ #     if x == Player.all.size
+ #       id_looper = 1
+ #     end
+ #   end
+ # end
 
-  def dies
-    Email.new.reassigning_email(assassin.email, victim.word, victim.name)
-    Email.new.dead_email(self.email, assassin.name)
-    self.update_attribute(:alive?, false)
-    assassin.increase_kills
-    reassign_victim_upon_successful_assassination
-    announce_winner if winner?
-  end
+ def dies
+   Email.new.reassigning_email(assassin.email, victim.word, victim.name)
+   Email.new.dead_email(self.email, assassin.name)
+   self.update_attribute(:alive?, false)
+   assassin.increase_kills
+   reassign_victim_upon_successful_assassination
+   announce_winner if winner?
+ end
 
-  def increase_kills
-    assassin.update_attribute(:kills, assassin.kills + 1)
-  end
+ def increase_kills
+   assassin.update_attribute(:kills, assassin.kills + 1)
+ end
 
-  def self.initial_victim_id_assignment
-    self.all.each do |player|
-      player.initial_victim_id_assignment
-      player.update_attribute(:alive?, true)
-    end
-  end
+ def self.initial_victim_id_assignment
+   self.all.each do |player|
+     player.initial_victim_id_assignment
+     player.update_attribute(:alive?, true)
+   end
+ end
 
-  def victim
-    Player.find_by id: victim_id
-  end
+ def victim
+   Player.find_by id: victim_id
+ end
 
-  def assassin
-    Player.find_by victim_id: id
-  end
+ def assassin
+   Player.find_by victim_id: id
+ end
 
-  def initial_victim_id_assignment
-    continued_victim_pool? ? choose_next_victim : loop_to_find_victim
-    Email.new.initial_email(self.email, victim.word, victim.name)
-  end
+ def initial_victim_id_assignment
+   continued_victim_pool? ? choose_next_victim : loop_to_find_victim
+   Email.new.initial_email(self.email, victim.word, victim.name)
+ end
 
-  def self.word_assignment
-    Player.all.each do |player|
-      player.update_attribute(:word, Word.word_list.shuffle[0])
-    end
-  end
+ def self.word_assignment
+   Player.all.each do |player|
+     player.update_attribute(:word, Word.word_list.shuffle[0])
+   end
+ end
 
-  def continued_victim_pool?
-    !!(self.id < Player.all.size)
-  end
+ def continued_victim_pool?
+   !!(self.id < Player.all.size)
+ end
 
-  def loop_to_find_victim
-    self.update_attribute(:victim_id, 1)
-  end
+ def loop_to_find_victim
+   self.update_attribute(:victim_id, 1)
+ end
 
-  def choose_next_victim
-    self.update_attribute(:victim_id, self.id + 1)
-  end
+ def choose_next_victim
+   self.update_attribute(:victim_id, self.id + 1)
+ end
 
-  # def initial_victim_id_assignment
-  #   if self.id < Player.all.size
-  #     self.update_attribute(:victim_id, self.id + 1)
-  #   else
-  #     self.update_attribute(:victim_id, 1)
-  #   end
-  #   #Email.initial_email(name,players_email,words)
-  # end
+ # def initial_victim_id_assignment
+ #   if self.id < Player.all.size
+ #     self.update_attribute(:victim_id, self.id + 1)
+ #   else
+ #     self.update_attribute(:victim_id, 1)
+ #   end
+ #   #Email.initial_email(name,players_email,words)
+ # end
 
-  def reassign_victim_upon_successful_assassination
-    assassin.update_attribute(:victim_id, self.victim_id)
-    self.update_attribute(:victim_id, nil)
-  end
+ def reassign_victim_upon_successful_assassination
+   assassin.update_attribute(:victim_id, self.victim_id)
+   self.update_attribute(:victim_id, nil)
+ end
 
-  def winner?
-    !!(Player.where(alive?: true).size == 1)
-  end
+ def winner?
+   !!(Player.where(alive?: true).size == 1)
+ end
 
-  def announce_winner
-    winner = Player.find_by(alive?: true).name
-    Player.all.each do |player|
-      Email.new.winner_email(player.email, winner)
-    end
-  end
+ def announce_winner
+   winner = Player.find_by(alive?: true).name
+   Player.all.each do |player|
+     Email.new.winner_email(player.email, winner)
+   end
+ end
 
 
 end
