56a07131333150b80afb29fd63c0b0f27ec041d0
supzann3 <susan.hehe01@gmail.com>
2015-08-22 14:14:21 -0400

    testing

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,27 +2,23 @@ require 'gmail'
 class Email #<<ActiveRecord::Base # not sure if we should create a table for email
  # belongs_to :player
 
-    def initialize
-      keys = YAML.load_file('keys.yml')
-      @gmail = Gmail.connect(keys['EMAIL'], keys['PASSWORD'])
-    end
-
-    def deliver_gmail(recipient, subject, body)
-      @gmail.deliver do
-        # binding.pry email=Mail.new
-        to "#{recipient}"
-        subject "#{subject}"
-        text_part do
-          body "#{body}"
-        end
-        # html_part do
-        #  content_type 'text/html; charset=UTF-8'
-        #  body "<p>Text of <em>html</em> message.</p>" # add html part
-      #  end
-        # add_file "/path/to/some_image.jpg" add image
+  def deliver_gmail(recipient, subject, body)
+  #  gmail=Gmail.connect("email", "password") do |gmail|
+    gmail.deliver do
+      # binding.pry email=Mail.new
+      to "#{recipient}"
+      subject "#{subject}"
+      text_part do
+        body "#{body}"
       end
+      html_part do
+       content_type 'text/html; charset=UTF-8'
+       body "<p>Text of <em>html</em> message.</p>" # add html part
+     end
+      # add_file "/path/to/some_image.jpg" add image
     end
-
+  # end
+  end
 
   def initial_email(player_email,word,victim) #comes from Player
     # players_email.each do |player| #either to a iteration at Player's class or pass an array here
@@ -33,7 +29,6 @@ class Email #<<ActiveRecord::Base # not sure if we should create a table for ema
       puts "Sent e-mail to #{player_email}."
     # end
   end
-
   def reassigning_email(player_email,word,victim) #comes from Player
       recipient = player_email
       subject = "Your new victim"
@@ -41,7 +36,6 @@ class Email #<<ActiveRecord::Base # not sure if we should create a table for ema
       deliver_gmail(recipient, subject, body)
       puts "Sent e-mail to #{player_email}."
   end
-
   def dead_email(player_email,killer_name) #comes from Player
     recipient = player_email
     subject = "Your have been killed"
@@ -49,11 +43,10 @@ class Email #<<ActiveRecord::Base # not sure if we should create a table for ema
     deliver_gmail(recipient, subject, body)
     puts "Sent e-mail to #{player_email}."
   end
-
-  def winner_email(player_email,winner_name) #comes from Player
+  def winner_email(player_email) #comes from Player
     recipient = player_email
-    subject = "The winner has been announced"
-    body = "The winner of the game is #{winner_name}"
+    subject = "You are a winner"
+    body = "You are the last suvivor, You are the ultimate Winner!"
     deliver_gmail(recipient, subject, body)
     puts "Sent e-mail to #{player_email}."
   end
